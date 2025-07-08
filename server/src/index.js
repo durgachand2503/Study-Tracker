20dc9669f0cd7f201940f587cb81fc7bc14cbc22
Durga Chand <gorantladurgachand@gmail.com>
2025-07-08 21:16:03 +0530

    Day 1: Scaffold client-server monorepo structure

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+connectDB();
+
+app.get('/', (req, res) => res.send('StudTrack API running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
