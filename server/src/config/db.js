dd3e93c01f670c9eddb8b193a67c6989e294741e
Durga Chand <gorantladurgachand@gmail.com>
2025-07-08 21:48:43 +0530

    Day 3: Setup environment variables with dotenv

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 module.exports = async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 connectDB();
 
 app.get('/', (req, res) => res.send('StudTrack API running'));
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'MongoDB is live ' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
